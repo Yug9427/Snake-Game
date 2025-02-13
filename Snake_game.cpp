b46e4920b4cc46542001ee943fe686eeec9c2d0c
Yug9427 <yugmalaviya9427@gmail.com>
2025-02-13 18:00:36 +0530

    Snake_game.cpp

--- Snake_game.cpp
+++ Snake_game.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
-#include <conio.h>
-#include <windows.h>
 #include <vector>
+#include <windows.h>
+#include <conio.h>  // For _kbhit() and _getch()
 
 using namespace std;
 
-enum eDirection { STOP, LEFT, RIGHT, UP, DOWN };
+const int width = 30, height = 20;
 
 class SnakeGame {
 private:
+    HANDLE console;
     bool gameOver;
-    const int width = 20;
-    const int height = 20;
-    int x, y, fruitX, fruitY, score;
-    vector<int> tailX, tailY;
-    eDirection dir;
-    
-public:
-    SnakeGame() {
-        Setup();
+    int x, y, foodX, foodY, score;
+    vector<pair<int, int>> snake;
+    enum Direction { STOP, LEFT, RIGHT, UP, DOWN };
+    Direction dir;
+
+    void GotoXY(int x, int y) {
+        COORD coord = { (SHORT)x, (SHORT)y };
+        SetConsoleCursorPosition(console, coord);
+    }
+
+    void HideCursor() {
+        CONSOLE_CURSOR_INFO cursorInfo;
+        GetConsoleCursorInfo(console, &cursorInfo);
+        cursorInfo.bVisible = false;
+        SetConsoleCursorInfo(console, &cursorInfo);
     }
 
     void Setup() {
@@ -26,118 +33,122 @@ public:
         dir = STOP;
         x = width / 2;
         y = height / 2;
-        fruitX = rand() % width;
-        fruitY = rand() % height;
+        foodX = rand() % width;
+        foodY = rand() % height;
         score = 0;
-        tailX.clear();
-        tailY.clear();
+        snake.clear();
+        snake.push_back({x, y});
+        HideCursor();
     }
 
     void Draw() {
-        COORD cursorPos = { 0, 0 };
-        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursorPos);
+        GotoXY(0, 0);
 
-        for (int i = 0; i < width + 2; i++) cout << "-";
+        for (int i = 0; i < width + 2; i++) cout << "# ";
         cout << endl;
 
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                if (j == 0) cout << "|";
-                if (i == y && j == x)
-                    cout << "O";
-                else if (i == fruitY && j == fruitX)
-                    cout << "*";
+                if (j == 0) cout << "# ";
+
+                if (i == y && j == x) cout << "O ";  // Head
+                else if (i == foodY && j == foodX) cout << "* ";  // Food
                 else {
                     bool print = false;
-                    for (size_t k = 0; k < tailX.size(); k++) {
-                        if (tailX[k] == j && tailY[k] == i) {
-                            cout << "o";
+                    for (auto s : snake) {
+                        if (s.first == j && s.second == i) {
+                            cout << "o ";
                             print = true;
+                            break;
                         }
                     }
-                    if (!print) cout << " ";
+                    if (!print) cout << "  ";
                 }
-                if (j == width - 1) cout << "|";
+
+                if (j == width - 1) cout << "# ";
             }
             cout << endl;
         }
 
-        for (int i = 0; i < width + 2; i++) cout << "-";
-        cout << endl;
-
-        cout << "Score: " << score << endl;
+        for (int i = 0; i < width + 2; i++) cout << "# ";
+        cout << "\nScore: " << score << endl;
     }
 
     void Input() {
         if (_kbhit()) {
             switch (_getch()) {
-            case 'a':
-                if (dir != RIGHT) dir = LEFT;
-                break;
-            case 'd':
-                if (dir != LEFT) dir = RIGHT;
-                break;
-            case 'w':
-                if (dir != DOWN) dir = UP;
-                break;
-            case 's':
-                if (dir != UP) dir = DOWN;
-                break;
-            case 'x':
-                gameOver = true;
-                break;
+                case 'a': case 'A': dir = LEFT; break;
+                case 'd': case 'D': dir = RIGHT; break;
+                case 'w': case 'W': dir = UP; break;
+                case 's': case 'S': dir = DOWN; break;
+                case 'x': gameOver = true; break;
             }
         }
     }
 
     void Logic() {
-        int prevX = (tailX.empty()) ? x : tailX[0];
-        int prevY = (tailY.empty()) ? y : tailY[0];
+        int prevX = snake[0].first, prevY = snake[0].second;
         int prev2X, prev2Y;
+        snake[0] = {x, y};
 
-        if (!tailX.empty()) {
-            tailX.insert(tailX.begin(), x);
-            tailY.insert(tailY.begin(), y);
-            tailX.pop_back();
-            tailY.pop_back();
+        for (size_t i = 1; i < snake.size(); i++) {
+            prev2X = snake[i].first;
+            prev2Y = snake[i].second;
+            snake[i] = {prevX, prevY};
+            prevX = prev2X;
+            prevY = prev2Y;
         }
 
         switch (dir) {
-        case LEFT:  x--; break;
-        case RIGHT: x++; break;
-        case UP:    y--; break;
-        case DOWN:  y++; break;
-        default:    break;
+            case LEFT: x--; break;
+            case RIGHT: x++; break;
+            case UP: y--; break;
+            case DOWN: y++; break;
+            default: break;
         }
 
-        if (x < 0 || x >= width || y < 0 || y >= height)
-            gameOver = true;
+        if (x < 0 || x >= width || y < 0 || y >= height) gameOver = true;
+        for (size_t i = 1; i < snake.size(); i++) {
+            if (snake[i].first == x && snake[i].second == y) gameOver = true;
+        }
 
-        for (size_t i = 0; i < tailX.size(); i++)
-            if (tailX[i] == x && tailY[i] == y)
-                gameOver = true;
-
-        if (x == fruitX && y == fruitY) {
+        if (x == foodX && y == foodY) {
             score += 10;
-            fruitX = rand() % width;
-            fruitY = rand() % height;
-            tailX.push_back(prevX);
-            tailY.push_back(prevY);
+            snake.push_back({foodX, foodY});
+
+            bool validFoodPosition;
+            do {
+                validFoodPosition = true;
+                foodX = rand() % width;
+                foodY = rand() % height;
+
+                for (const auto& segment : snake) {
+                    if (segment.first == foodX && segment.second == foodY) {
+                        validFoodPosition = false;
+                        break;
+                    }
+                }
+            } while (!validFoodPosition);
         }
     }
 
-    void GameLoop() {
+public:
+    SnakeGame() : console(GetStdHandle(STD_OUTPUT_HANDLE)) {}
+
+    void Run() {
+        Setup();
         while (!gameOver) {
             Draw();
             Input();
             Logic();
-            Sleep(100); // Adjust game speed here
+            Sleep(100);
         }
+        cout << "\nGame Over! Final Score: " << score << endl;
     }
 };
 
 int main() {
     SnakeGame game;
-    game.GameLoop();
+    game.Run();
     return 0;
 }
